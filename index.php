9bea24ca8986c84a6139cacd9bd541fc6f700a84
Izzatbek <58590849+Senvamen@users.noreply.github.com>
2020-03-19 09:31:15 +0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
-echo 'File not found!';
+$token = '1092997171:AAF-VJTKJibNxmgvdGFd5VSzP8W4c-QDEOc';
+
+function bot($method,$datas=[]) {
+global $token;
+$url = "https://api.telegram.org/bot".$token."/".$method;
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL,$url);
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
+$res = curl_exec($ch);
+if (curl_error($ch)) {
+var_dump(curl_error($ch));
+} else {
+return json_decode($res);
+}
+}
+
+$update = json_decode(file_get_contents('php://input'));
+$message = $update->message;
+$text = $message->text;
+$message_id = $message->message_id;
+$chat_id = $message->chat->id;
+$user_id = $message->from->id;
+$name = $message->from->first_name;
+
+if ($text == "/start") {
+bot('sendMessage',[
+'chat_id'=> $chat_id,
+'text'=> "Hello world!",
+'parse_mode'=> 'HTML',
+]);
+}
