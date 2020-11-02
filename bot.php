364bd1f979a45e1d877c20e2b4c3d28d8e659e0b
Izzatbek <58590849+izzatbekk@users.noreply.github.com>
2020-11-02 20:12:07 +0500

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,19 +1,19 @@
 <?php
 
-$token = 'BOT_TOKEN';
+$HTTP_API = '1111802874:AAEoK8Xqp1dP3DqjBEMIqVuPyGjFCcL43M4';
 
 function bot($method, $datas = []) {
-global $token;
-$url = "https://api.telegram.org/bot".$token."/".$method;
+global $HTTP_API;
+$url = 'https://api.telegram.org/bot' . $HTTP_API . '/'. $method;
 $ch = curl_init();
-curl_setopt($ch,CURLOPT_URL,$url);
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-curl_setopt($ch,CURLOPT_POSTFIELDS,$datas);
-$res = curl_exec($ch);
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $datas);
+$result = curl_exec($ch);
 if (curl_error($ch)) {
 var_dump(curl_error($ch));
 } else {
-return json_decode($res);
+return json_decode($result);
 }
 }
 
@@ -23,18 +23,18 @@ $text = $message->text;
 $message_id = $message->message_id;
 $chat_id = $message->chat->id;
 
-if ($text == "/start" or $text == "/start@Joinhider2_bot" or $text == "/start@Joinhider2_bot start") {
+if ($text == '/start' or $text == '/start@Joinhider2_bot' or $text == '/start@Joinhider2_bot start') {
 bot('sendMessage',[
-'chat_id'=> $chat_id,
-'text'=> "*Joinhider2_bot* version: `1.1.3`
+'chat_id' => $chat_id,
+'text' => "*Joinhider2_bot* version: `1.0.4`
 
 Bot to remove messages about user joined or left chatroom.
 
 Add it to your group for bot operation, then assign it as an administrator.",
-'parse_mode'=> 'markdown',
-'reply_markup'=> json_encode([
-'inline_keyboard'=> [
-[['text'=> "➕ Add to group ➕", 'url'=> "https://t.me/joinhider2_bot?startgroup=start"]]
+'parse_mode' => 'markdown',
+'reply_markup' => json_encode([
+'inline_keyboard' => [
+[['text' => '➕ Add to group ➕', 'url' => 'https://t.me/joinhider2_bot?startgroup=start']]
 ]
 ])
 ]);
@@ -42,13 +42,13 @@ Add it to your group for bot operation, then assign it as an administrator.",
 
 if ($message->new_chat_member) {
 bot('deleteMessage',[
-'chat_id'=> $chat_id,
-'message_id'=> $message_id
+'chat_id' => $chat_id,
+'message_id' => $message_id
 ]);
 }
 if ($message->left_chat_member) {
 bot('deleteMessage',[
-'chat_id'=> $chat_id,
-'message_id'=> $message_id
+'chat_id' => $chat_id,
+'message_id' => $message_id
 ]);
 }
